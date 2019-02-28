35bf87f430aa8ad72143e04827ab4e922bef34be
Lara Marie Reimer <laramarie@reimer.io>
2019-02-28 10:57:46 +0100

    Bugfix (sessionid & userId) & version bump

--- CUU/Classes/InteractionKit/IKAppEvent/IKDefaultAppEvent.swift
+++ CUU/Classes/InteractionKit/IKAppEvent/IKDefaultAppEvent.swift
@@ -1,7 +1,7 @@
 public class IKDefaultAppEvent: IKAppEvent {
     
-    public var _session: String = ""
-    public var _user: String = ""
+    public var _session: String = CUUSessionManager.sharedManager.currentSession
+    public var _user: String = CUUUserManager.sharedManager.userId
     public var startDate: Date = Date()
     public var endDate: Date = Date()
     public var title: String = ""
--- CUU/Classes/InteractionKit/IKDevice/IKDefaultDevice.swift
+++ CUU/Classes/InteractionKit/IKDevice/IKDefaultDevice.swift
@@ -1,7 +1,7 @@
 public class IKDefaultDevice: IKDevice {
     
-    public var _session: String = ""
-    public var _user: String = ""
+    public var _session: String = CUUSessionManager.sharedManager.currentSession
+    public var _user: String = CUUUserManager.sharedManager.userId
     public var startDate: Date = Date()
     public var endDate: Date = Date()
     public var title: String = ""
